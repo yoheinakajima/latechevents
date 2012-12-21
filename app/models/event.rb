02a277ca7231dfd6b9f77917b28fcfb814c25703
Yohei Nakajima <me@yoheinakajima.com>
2012-12-20 17:19:08 -0800

    Added starttime and endtime

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ActiveRecord::Base
-  attr_accessible :name, :website
+  attr_accessible :name, :website, :starttime, :endtime
   
   validates :name, :presence => true
   validates :website, :presence => true
+  validates :starttime, :presence => true
+  validates :endtime, :presence => true
+  
+  default_scope order("starttime ASC")
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121220232801) do
+ActiveRecord::Schema.define(:version => 20121221004628) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.string   "website"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.datetime "starttime"
+    t.datetime "endtime"
   end
 
 end
