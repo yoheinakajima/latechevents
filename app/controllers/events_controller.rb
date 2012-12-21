fe7964c04dff34c4c77ce53e7f8206edf2f38323
Yohei Nakajima <me@yoheinakajima.com>
2012-12-20 19:59:59 -0800

    Only show events that end now or later. Using Controller conditions in index.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all(:conditions => ["endtime > ?", Time.now])
 
     respond_to do |format|
       format.html # index.html.erb
